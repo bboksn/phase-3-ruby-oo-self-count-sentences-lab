b6c55ecc12342b14888279609d3847c57fc61c8d
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-09-29 11:50:29 -0400

    commit tree 87955baec328045d42ab74dab35b64aeee4a7465

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true:false
   end
 
   def question?
-
+    self.end_with?("?") ? true:false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true:false
   end
 
   def count_sentences
+   #self.split.each_cons(2).map{|char| char.pop if char[0] == char[1] }.filter {|punc| punc = "." && "?" && "!"}.count
+    #self.split.map{|char,index| delete[char] if char == char[index+1]}.filter {|punc| punc = "." && "?" && "!"}.count
+   ## self.split.filter {|punc| punc = "." && "?" && "!"}.count
+
+   #self.split.each_cons(2).filter {|punc| punc = "." && "?" && "!"}.count
+   #self.scan(/((.)\2*)/).map(&:first).filter {|punc| punc = "." && "?" && "!"}.count
+   self.split(/\.|\?|!/).filter { |s| !s.empty? }.count
+
 
   end
 end
